63676225a9133ab164a78acf0e694c67a286bcd4
Boburjonbest <tursunovb746@gmail.com>
2024-11-22 16:23:31 +0500

    changes

--- Controller/Localizardtretiystoron.cs
+++ Controller/Localizardtretiystoron.cs
@@ -1,12 +1,51 @@
+using Localizard.DAL;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Localizard.Controller;
 
-public class Localizardtretiystoron : Controller
+public class Localizardtretiystoron : ControllerBase
 {
-    // GET
-    public IActionResult Index()
+    private readonly AppDbContext _context;
+
+    public Localizardtretiystoron(AppDbContext context)
     {
-        return View();
+        _context = context;
     }
+
+    [HttpGet("Apitretiystoron")]
+    public async Task<IActionResult> Getproject([FromQuery] string Name)
+    {
+        var projectInfo = await _context.Projects
+            .Where(p => p.Name == Name)
+            .FirstOrDefaultAsync();
+
+        if (projectInfo == null)
+        {
+            return NotFound("Project not found");
+        }
+
+        var projectDetail = await _context.ProjectDetails
+            .Where(p => p.TranslationId == projectInfo.Id)
+            .Include(p => p.Translation)
+            .ToListAsync();
+
+        if (projectDetail == null || !projectDetail.Any())
+        {
+            return NotFound("No project details found");
+        }
+
+        var result = projectDetail.Select(p => new
+        {
+            id = p.Id,
+            name = p.Key,
+            translationId = p.TranslationId,
+            translation = p.Translation.Languages,
+            text = p.Translation.Text,
+        }).ToList();
+    
+        return Ok(result);
+
+    }
+   
 }
